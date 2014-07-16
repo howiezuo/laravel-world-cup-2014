48ba75d2bd6e7aa4c9cedffb8972b79bd97e4609
Hao Zuo <etsushi.sa@gmail.com>
2014-07-16 20:13:40 +0900

    env

---
+++ .env.php
@@ -0,0 +1,10 @@
+<?php
+
+//mysql://b24ff94a8b4ce2:4f57b9a5@us-cdbr-east-06.cleardb.net/heroku_3240344dbe568c6?reconnect=true
+return array(
+	// Database
+	'MY_DB_HOST'	=> 'us-cdbr-east-06.cleardb.net',
+	'MY_DB_NAME'	=> 'heroku_3240344dbe568c6',
+	'MY_DB_USER'	=> 'b24ff94a8b4ce2',
+	'MY_DB_PASS'	=> '4f57b9a5'
+);
